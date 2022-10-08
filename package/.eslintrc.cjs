4d1c72af1d55a47c4c180ea849e088eb4384e001
infodusha <infodusha@ya.ru>
2022-10-08 16:38:03 +0300

    feat(app): move to package directory

---
+++ package/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  rules: {
+    '@typescript-eslint/no-namespace': 'off'
+  }
+}
+
