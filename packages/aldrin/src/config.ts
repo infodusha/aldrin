3e58ea62edff27b4f5f2d556a6590b8ff57d12a5
infodusha <infodusha@ya.ru>
2022-11-24 21:03:08 +0300

    feat(core): config has defaults

--- packages/aldrin/src/config.ts
+++ packages/aldrin/src/config.ts
@@ -1,10 +1,20 @@
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
 
-interface Config {
-  port: number;
+class Config {
+  readonly port: number = 8089;
+  readonly connectionTimeoutMs: number = 30000;
 }
 
-const content = readFileSync(join(process.cwd(), '/.aldrinrc'), 'utf8');
+const defaults = new Config();
 
-export const config: Config = JSON.parse(content);
+const handler: ProxyHandler<Config> = {
+  get(target, prop: keyof Config) {
+    return target[prop] ?? defaults[prop];
+  },
+};
+
+const content = readFileSync(join(process.cwd(), '/.aldrinrc'), 'utf8');
+const source: Config = JSON.parse(content);
+
+export const config: Config = new Proxy(source, handler);
