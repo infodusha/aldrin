0c189c842a83e83aa48a935c8d414e0e8ccda999
infodusha <infodusha@ya.ru>
2022-12-11 20:40:17 +0300

    feat(core): config refactor + optional

--- packages/aldrin/src/config.ts
+++ packages/aldrin/src/config.ts
@@ -4,17 +4,42 @@ import { join } from 'node:path';
 class Config {
   readonly port: number = 8089;
   readonly connectionTimeoutMs: number = 30000;
+
+  static withDefaults(source: Config): Config {
+    const defaults = new Config();
+
+    const handler: ProxyHandler<Config> = {
+      get(target, prop: keyof Config) {
+        return target[prop] ?? defaults[prop];
+      },
+    };
+
+    try {
+      return new Proxy(source, handler);
+    } catch {
+      throw new Error('Unable to read config: invalid format (not an object)');
+    }
+  }
+
+  static fromString(content: string): Config {
+    let source: Config;
+    try {
+      source = JSON.parse(content);
+    } catch {
+      throw new Error('Unable to read config: invalid format (not a JSON)');
+    }
+
+    return Config.withDefaults(source);
+  }
+
+  static fromFile(path: string): Config {
+    let content: string;
+    try {
+      content = readFileSync(join(process.cwd(), path), 'utf8');
+    } catch {
+      return new Config();
+    }
+    return Config.fromString(content);
+  }
 }
-
-const defaults = new Config();
-
-const handler: ProxyHandler<Config> = {
-  get(target, prop: keyof Config) {
-    return target[prop] ?? defaults[prop];
-  },
-};
-
-const content = readFileSync(join(process.cwd(), '/.aldrinrc'), 'utf8');
-const source: Config = JSON.parse(content);
-
-export const config: Config = new Proxy(source, handler);
+export const config: Config = Config.fromFile('.aldrinrc');
