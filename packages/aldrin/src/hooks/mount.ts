4aec8dbb5321c4444899ddbae8744dc9a04b9e32
infodusha <infodusha@ya.ru>
2022-12-11 20:28:35 +0300

    fix(core): make mount hook simple

--- packages/aldrin/src/hooks/mount.ts
+++ packages/aldrin/src/hooks/mount.ts
@@ -1,4 +1,4 @@
-import { renderContext, UserContext, userContext } from '../context';
+import { renderContext } from '../context';
 
 type MountFn = (() => void) | (() => () => void);
 
@@ -9,34 +9,23 @@ export function useMount(fn: MountFn): void {
 
 export class Mount {
   private readonly mounts = new Set<MountFn>();
-  private readonly unMounts = new WeakMap<UserContext, Set<() => void>>();
+  private readonly unMounts = new Set<() => void>();
 
   add(fn: MountFn): void {
     this.mounts.add(fn);
   }
 
   mount(): void {
-    const uContext = userContext.get();
     this.mounts.forEach((fn) => {
       const unMount = fn();
       if (unMount != null) {
-        let unMounts = this.unMounts.get(uContext);
-        if (unMounts == null) {
-          unMounts = new Set<() => void>();
-          this.unMounts.set(uContext, unMounts);
-        }
-        unMounts.add(unMount);
+        this.unMounts.add(unMount);
       }
     });
   }
 
   unMount(): void {
-    const uContext = userContext.get();
-    const unMounts = this.unMounts.get(uContext);
-    if (unMounts == null) {
-      return;
-    }
-    unMounts.forEach((fn) => fn());
-    unMounts.clear();
+    this.unMounts.forEach((fn) => fn());
+    this.unMounts.clear();
   }
 }
