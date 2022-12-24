470a3739de3e28ac5784c00ef436d3d742c1597a
infodusha <infodusha@ya.ru>
2022-12-24 14:12:17 +0300

    fix(test): a try to update vitest 3

--- packages/aldrin/vitest.config.ts
+++ packages/aldrin/vitest.config.ts
@@ -4,9 +4,8 @@ export default defineConfig({
   test: {
     watchExclude: ['**/node_modules/**', '**/dist/**', 'README.md'],
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    deps: {
-      inline: true,
-      interopDefault: true,
+    alias: {
+      'aldrin/jsx-runtime': './src/jsx-runtime.ts',
     },
   },
 });
